650de2de45a760f7723dfe0cb46aa01639639549
albortas <garlad628@gmail.com>
2025-02-16 09:24:04 -0400

    terminamos con la biblioteca herramientas

--- herramientas/sistema.py
+++ herramientas/sistema.py
@@ -13,21 +13,24 @@ class Singleton(type):
         return cls._instances[cls]
 
 
-class System(metaclass=Singleton):
+class Sistema(metaclass=Singleton):
 
     def __init__(self):
 
         try:
-            log.debug('Loading system...')
+            log.debug('Cargando sistema...')
 
         except Exception as e:
-            log.error('Problem while loading the configuration file', e)
+            log.error('Problema al cargar el archivo sistema', e)
 
-    def temperature(self):
+    def temperatura(self):
         try:
-            temp = os.popen("vcgencmd measure_temp").readline()
+            temp = os.popen("vcgencmd measure_temp").readline() # Comando para obtener la temperatura del sistema
             # log.debug("System temperature: " + temp.replace("temp=", "")[:-5])
             return temp.replace("temp=", "")[:-5]
         except:
             return '000'
 
+if __name__ == '__main__':
+    sistema = Sistema()
+    print(sistema.temperatura())
